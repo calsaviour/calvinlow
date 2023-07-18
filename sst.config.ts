af545957af6c47cd5d734f1f30b318b6bb857922
Calvin Low <calsaviour@gmail.com>
2023-07-19 00:15:24 +0800

    Update custom domain

--- sst.config.ts
+++ sst.config.ts
@@ -4,16 +4,28 @@ import { NextjsSite } from "sst/constructs";
 export default {
   config(_input) {
     return {
-      name: "dev-site",
+      name: "calvinlow",
       region: "us-east-1",
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        customDomain: {
+          domainName:
+            stack.stage === "prod"
+              ? "calvin-low.com"
+              : `${stack.stage}.calvin-low.com`,
+          domainAlias:
+            stack.stage === "prod"
+              ? "www.calvin-low.com"
+              : `www.${stack.stage}.calvin-low.com`,
+          hostedZone: "calvin-low.com",
+        }
+      });
 
       stack.addOutputs({
-        SiteUrl: site.url,
+        url: site.url
       });
     });
   },
